c7ab48f05297e3ae910886d83aef3ca1f33b5c05
Jeff Fredenburg <136350727+jfredenburg@users.noreply.github.com>
2026-01-14 09:42:40 -0600

    refactor: update dependency URLs to asikros organization
    - Change all github.com/ic-designer URLs to github.com/asikros
    - Ensures dependencies point to correct repository organization

--- make/deps.mk
+++ make/deps.mk
@@ -11,20 +11,20 @@ include src/bowerbird-deps/bowerbird-deps.mk
 $(call bowerbird::git-dependency, \
     name=bowerbird-help, \
     path=$(WORKDIR_DEPS)/bowerbird-help, \
-    url=https://github.com/ic-designer/make-bowerbird-help.git, \
+    url=https://github.com/asikros/make-bowerbird-help.git, \
     branch=main, \
     entry=bowerbird.mk)
 
 $(call bowerbird::git-dependency, \
     name=bowerbird-githooks, \
     path=$(WORKDIR_DEPS)/bowerbird-githooks, \
-    url=https://github.com/ic-designer/make-bowerbird-githooks.git, \
+    url=https://github.com/asikros/make-bowerbird-githooks.git, \
     branch=main, \
     entry=bowerbird.mk)
 
 $(call bowerbird::git-dependency, \
     name=bowerbird-test, \
     path=$(WORKDIR_DEPS)/bowerbird-test, \
-    url=https://github.com/ic-designer/make-bowerbird-test.git, \
+    url=https://github.com/asikros/make-bowerbird-test.git, \
     branch=main, \
     entry=bowerbird.mk)
